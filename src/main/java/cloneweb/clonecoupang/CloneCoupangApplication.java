1ed42ed2eb8e1bffdd1a4afa78eb057498406f79
HyunSBong <sanseng@mju.ac.kr>
2021-02-02 23:35:20 +0900

    init

---
+++ src/main/java/cloneweb/clonecoupang/CloneCoupangApplication.java
@@ -0,0 +1,13 @@
+package cloneweb.clonecoupang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneCoupangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneCoupangApplication.class, args);
+	}
+
+}
